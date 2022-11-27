6e9a538bdcbc9e924d29f8cda6b94a368e62d7fd
kraskoval <kr.muratovski@gmail.com>
2022-11-27 19:43:03 +0200

    SECRET KEY change

--- fragmantica/settings.py
+++ fragmantica/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--doql7^!4%=jdjk5i!i+eptszx_awz31kpwb1xj_f2*)876%v0'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
